e2d7758abc5010fda242821bbe5b9c5b2e4c6253
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-07-06 21:01:11 +0300

    Fix table size and scrolls

--- src/components/data-table/data-table.svelte
+++ src/components/data-table/data-table.svelte
@@ -64,113 +64,36 @@
     });
 </script>
 
-<Grid style="width: 100%; height:100%">
+<TableContainer>
     <div {id} bind:this={ref} {...$$restProps} class={tableClasses} style={$$restProps.style}>
-        {#if waiting}
-            <Preloader showBackground size="default" style="margin-bottom:0;" />
-        {/if}
-
-        <Row direction="row" gap={'1'} alignItems="stretch">
-            {#if showSearch}
-                <Column grow={1}>
-                    <SearchInput bind:value={searchValue} label="Search"><IconButton on:click={globalSearch} icon={Search} /></SearchInput>
-                </Column>
+        <Flex wrap="nowrap" direction="column" style="position:absolute; left:0; right:0; top:0; bottom:0;" gap="2">
+            {#if waiting}
+                <Preloader showBackground size="default" style="margin-bottom:0;" />
             {/if}
-            {#if showFilters}
-                <Column grow={0}>
-                    <Menu type="flat" rounded icon={Filter} listModel={list} useIconButton position="bottom-right" />
-                </Column>
-            {/if}
-        </Row>
 
-        {#if table && table.rows && table.rows.length === 0}
-            <Row wrap="nowrap" justifyContent="center" alignItems="center">
-                <Column>
-                    <Title heading="6" color="secondary">No record found!</Title>
-                </Column>
+            <Row direction="row" gap={'1'} alignItems="stretch">
+                {#if showSearch}
+                    <Column grow={1}>
+                        <SearchInput bind:value={searchValue} label="Search"
+                            ><IconButton on:click={globalSearch} icon={Search} /></SearchInput>
+                    </Column>
+                {/if}
+                {#if showFilters}
+                    <Column grow={0}>
+                        <Menu type="flat" rounded icon={Filter} listModel={list} useIconButton position="bottom-right" />
+                    </Column>
+                {/if}
             </Row>
-        {:else}
-            <FlexItem grow={1} style="overflow-x:auto; overflow-y:auto">
-                <table style="width:100%; height:100%">
-                    {#if table.headers}
-                        <thead>
-                            <TableHead {condense}>
-                                {#each table.headers as header, i}
-                                    <TableHeadCell sortable={header.sortable} align={header.align}>
-                                        <Row gap="4" wrap="nowrap" alignItems="center" justifyContent="space-between">
-                                            <Column grow={1}><Span>{header.value}</Span></Column>
-                                            {#if showColumnVisibility}
-                                                <Column>
-                                                    <IconButton
-                                                        on:click={() => {
-                                                            table.hideColumn(header);
-                                                        }}
-                                                        size="small"
-                                                        icon={View} />
-                                                </Column>
-                                            {/if}
-                                            {#if showColumnSearch}
-                                                <Column>
-                                                    <IconButton icon={Search} size="small" />
-                                                </Column>
-                                            {/if}
-                                        </Row>
-                                    </TableHeadCell>
-                                {/each}
-                            </TableHead>
-                        </thead>
-                    {/if}
-                    {#if table.rows}
-                        <tbody>
-                            {#each table.rows as row, j}
-                                <TableRow {condense} {useStrip} {hasDivider} odd={j % 2 === 1}>
-                                    {#each row.items as cell, k}
-                                        <TableCell align={table.headers[k].align}>
-                                            <Span>{cell.value}</Span>
-                                        </TableCell>
-                                    {/each}
-                                </TableRow>
-                            {/each}
-                        </tbody>
-                    {/if}
-                </table>
-            </FlexItem>
-        {/if}
-        {#if showPagination}
-            <FlexItem>
-                <Pagination />
-            </FlexItem>
-        {/if}
-    </div>
-</Grid>
-<!--  <div {id} bind:this={ref} {...$$restProps} class={tableClasses} style={$$restProps.style}>
-        {#if waiting}
-            <Preloader showBackground size="default" style="margin-bottom:0;" />
-        {/if}
 
-        <Row direction="row" gap={'1'} alignItems="stretch">
-            {#if showSearch}
-                <Column grow={1}>
-                    <SearchInput bind:value={searchValue} label="Search"><IconButton on:click={globalSearch} icon={Search} /></SearchInput>
-                </Column>
-            {/if}
-            {#if showFilters}
-                <Column grow={0}>
-                    <Menu type="flat" rounded icon={Filter} listModel={list} useIconButton position="bottom-right" />
-                </Column>
-            {/if}
-        </Row>
-
-        {#if table && table.rows && table.rows.length === 0}
-            <Row wrap="nowrap" justifyContent="center" alignItems="center">
-                <Column>
-                    <Title heading="6" color="secondary">No record found!</Title>
-                </Column>
-            </Row>
-        {:else}
-            <FlexItem grow={1}>
-                <TableContainer>
-                    <table>
+            {#if table && table.rows && table.rows.length === 0}
+                <Row wrap="nowrap" justifyContent="center" alignItems="center">
+                    <Column>
+                        <Title heading="6" color="secondary">No record found!</Title>
+                    </Column>
+                </Row>
+            {:else}
+                <FlexItem grow={1} style="overflow-x:auto; overflow-y:auto">
+                    <table style="width:100%; height:100%">
                         {#if table.headers}
                             <thead>
                                 <TableHead {condense}>
@@ -213,10 +136,13 @@
                             </tbody>
                         {/if}
                     </table>
-                </TableContainer>
-            </FlexItem>
-            {#if showPagination}
-                <Pagination />
+                </FlexItem>
             {/if}
-        {/if}
-    </div> -->
+            {#if showPagination}
+                <FlexItem>
+                    <Pagination />
+                </FlexItem>
+            {/if}
+        </Flex>
+    </div>
+</TableContainer>
