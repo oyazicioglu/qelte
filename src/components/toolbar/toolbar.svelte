70a5a6f8682baa40d7bc4a629fdab7ada4311e8b
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-07-13 06:29:53 +0300

    Add initial toolbar

---
+++ src/components/toolbar/toolbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Flex from '../flex/flex.svelte';
+
+    import type { BaseColor, BaseSize, FlexDirection } from '../types';
+
+    export let size: BaseSize = 'default';
+    export let color: BaseColor = 'inherit';
+    export let disabled: boolean = false;
+    export let rounded: boolean = false;
+    export let ref: HTMLElement = undefined;
+    export let direction: FlexDirection = 'row';
+
+    $: classes = [`qei-select`, `size-${size}`, `color-${color}`, disabled && `disabled`, rounded && `rounded`, $$restProps.class]
+        .filter(Boolean)
+        .join(' ');
+    $: styles = [$$restProps.style].filter(Boolean).join(';');
+</script>
+
+<Flex {ref} {...$$restProps} {direction} class={classes} style={styles}>
+    <slot />
+</Flex>
